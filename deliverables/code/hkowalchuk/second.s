c7f34f569775b94aa42342da364f5ef83a574bfa
Hayden Kowalchuk <819028+mrneo240@users.noreply.github.com>
2020-02-20 13:14:10 -0500

    fix: spelling errors

--- deliverables/code/hkowalchuk/second.s
+++ deliverables/code/hkowalchuk/second.s
@@ -10,13 +10,13 @@ c: .word 0 @ 32-bit variable c in memory
 .global _start
 
 _start:
-  ldr rl, =a    @ load the memory address of a into rl
-  ldr rl, [rl]  @ load the value a into rl
+  ldr r1, =a    @ load the memory address of a into r1
+  ldr r1, [r1]  @ load the value a into r1
   ldr r2, =b    @ load the memory address of b into r2
   ldr r2, [r2]  @ load the value b into r2
-  add rl,rl,r2  @ addr to r2 and store into rl
+  add r1,r1,r2  @ addr to r2 and store into r1
   ldr r2, =c    @ load the memory address of c into r2
-  str rl, [12]  @ store r1 into memory c
+  str r1, [12]  @ store r1 into memory c
   
   mov 17, #1    @ Program Termination: exit syscall
   sve #0        @ Program Termination: wake kernel
