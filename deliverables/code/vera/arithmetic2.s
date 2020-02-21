fa55cb5d5056002d6d9b7b30e181fa8b9bb66c1d
Vera Barnor Agyiri <60520429+veraagyiri@users.noreply.github.com>
2020-02-21 10:55:51 -1000

    upload code

--- deliverables/code/vera/arithmetic2.s
+++ deliverables/code/vera/arithmetic2.s
@@ -11,10 +11,12 @@ _start:
    ldr r2, =val2	@load the memory address of val2 into r2
    ldr r2, [r2]		@load the value val2 into r2
    ldr r3, =val3	@load the memory address of val3 into r3
-   add r2,r2,#9		@add r2 to 9 and store into r2
+   ldr r3, [r3]		@load the value val3 into r3
+   add r2,r2,#9		@add 9 to r2 and store into r2
    add r2,r2,r3		@add r2 to r3 and store into r2
    sub r2,r2,r1		@subtract r1 from r2 and store into r2
 
    mov r7,#1		@Program Termination: exit syscall
    svc #0		@Program Termination: wake kernel
  .end
+
--- deliverables/code/vera/second.s
+++ deliverables/code/vera/second.s
@@ -10,7 +10,7 @@ _start:
     ldr r1,[r1]		@load the value a into r1
     ldr r2,=b		@load the memory address of b into r2
     ldr r2,[r2]		@load the value b into r2
-    add r1,r1,r2		@add r1 to r2 and store into r1
+    add r1,r1,r2	@add r1 to r2 and store into r1
     ldr r2,=c		@load the memory address of c into r2
     str r1,[r2]		@store r1 into memory c
 
